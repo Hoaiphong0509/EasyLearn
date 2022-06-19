de259c49a1f3ca62cd388f6ecf1d5c549a315a9f
Hoai Phong <42806464+Hoaiphong0509@users.noreply.github.com>
2022-06-19 15:01:50 +0700

    Add new Proxy Endpoint

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,23 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
       target: 'http://localhost:5000',
-      changeOrigin: true,
+      changeOrigin: true
     })
-  );
-};
+  )
+  app.use(
+    createProxyMiddleware('/api', {
+      target: 'https://easy-learn.vercel.app/',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': ''
+      },
+      headers: {
+        Connection: 'keep-alive'
+      }
+    })
+  )
+}
